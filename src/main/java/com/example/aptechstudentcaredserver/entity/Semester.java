2411d777a9d558bfe48a87b9d46999c940e19728
Mihpham <minhpham1534@gmail.com>
2024-08-21 12:28:52 +0700

    add @NoArgsConstructor annotation in Semester

--- src/main/java/com/example/aptechstudentcaredserver/entity/Semester.java
+++ src/main/java/com/example/aptechstudentcaredserver/entity/Semester.java
@@ -3,12 +3,14 @@ package com.example.aptechstudentcaredserver.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "semesters")
 public class Semester {
     @Id
