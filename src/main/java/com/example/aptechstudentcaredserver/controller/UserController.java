5cd3f05b41217a3a1fd6480371581550611ac91d
buithihoi <hoi.bt.2156@aptechlearning.edu.vn>
2024-08-29 12:30:48 +0700

    Cloudinary

--- src/main/java/com/example/aptechstudentcaredserver/controller/UserController.java
+++ src/main/java/com/example/aptechstudentcaredserver/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.example.aptechstudentcaredserver.controller;
 
+import com.example.aptechstudentcaredserver.bean.request.ImageRequest;
 import com.example.aptechstudentcaredserver.bean.response.UserResponse;
+import com.example.aptechstudentcaredserver.entity.User;
 import com.example.aptechstudentcaredserver.exception.NotFoundException;
+import com.example.aptechstudentcaredserver.repository.UserRepository;
+import com.example.aptechstudentcaredserver.service.CloudinaryService;
 import com.example.aptechstudentcaredserver.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,6 +23,8 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
+    private final CloudinaryService cloudinaryService;
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers() {
@@ -45,4 +54,30 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
+    @PatchMapping("/{id}/image")
+    public ResponseEntity<?> updateImage(@PathVariable Long id, @RequestParam("image") MultipartFile imageFile) {
+        Optional<User> optionalUser = userRepository.findById(Math.toIntExact(id));
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            try {
+                // Tải tệp lên Cloudinary và lấy URL
+                String imageUrl = cloudinaryService.uploadToCloudinary(imageFile);
+
+                // Cập nhật URL ảnh vào đối tượng UserDetail
+                user.getUserDetail().setImage(imageUrl);
+
+                // Lưu User đã cập nhật vào cơ sở dữ liệu
+                userRepository.save(user);
+
+                return ResponseEntity.ok("Image updated successfully");
+            } catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload image");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+    }
+
+
+
 }
--- src/test/java/com/example/aptechstudentcaredserver/service/StudentServiceTest.java
+++ src/test/java/com/example/aptechstudentcaredserver/service/StudentServiceTest.java
@@ -1,2 +1,7 @@
-package com.example.aptechstudentcaredserver.service;public class StudentServiceTest {
+package com.example.aptechstudentcaredserver.service;
+
+
+class StudentServiceTest {
+
+
 }
