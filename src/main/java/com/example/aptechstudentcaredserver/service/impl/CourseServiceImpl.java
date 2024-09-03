b33c753ea9acc2593cf68d538db843c421ad99a5
Mihpham <minhpham1534@gmail.com>
2024-09-04 00:02:49 +0700

    Course CRUD API

--- src/main/java/com/example/aptechstudentcaredserver/service/impl/CourseServiceImpl.java
+++ src/main/java/com/example/aptechstudentcaredserver/service/impl/CourseServiceImpl.java
@@ -1,35 +1,76 @@
 package com.example.aptechstudentcaredserver.service.impl;
 
 import com.example.aptechstudentcaredserver.bean.request.CourseRequest;
+import com.example.aptechstudentcaredserver.bean.response.CourseResponse;
 import com.example.aptechstudentcaredserver.entity.Course;
+import com.example.aptechstudentcaredserver.exception.NotFoundException;
 import com.example.aptechstudentcaredserver.repository.CourseRepository;
 import com.example.aptechstudentcaredserver.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CourseServiceImpl implements CourseService {
     private final CourseRepository courseRepository;
 
+
     @Override
-    public void addCourse(CourseRequest courseRequest) {
-        Course existingCourse = courseRepository.findByCourseName(courseRequest.getCourseName());
-        if (existingCourse != null) {
-            throw new RuntimeException("Course with this name already exists");
-        }
+    public List<CourseResponse> getAllCourses() {
+        return courseRepository.findAll().stream()
+                .map(this::convertToCourseResponse)
+                .collect(Collectors.toList());
+    }
 
-        // Create new Course
-        Course newCourse = new Course();
-        newCourse.setCourseName(courseRequest.getCourseName());
-        newCourse.setCourseCode(courseRequest.getCourseCode());
-        newCourse.setClassSchedule(courseRequest.getClassSchedule());
-        newCourse.setCreatedAt(LocalDateTime.now());
-        newCourse.setUpdatedAt(LocalDateTime.now());
+    @Override
+    public CourseResponse getCourseById(int id) {
+        Course course = courseRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Course not found"));
+        return convertToCourseResponse(course);
+    }
 
-        // Save new Course
-        courseRepository.save(newCourse);
+    @Override
+    public void createCourse(CourseRequest courseRequest) {
+        Course course = new Course();
+        course.setCourseName(courseRequest.getCourseName());
+        course.setCourseCode(courseRequest.getCourseCode());
+        course.setClassSchedule(courseRequest.getClassSchedule());
+        course.setCourseCompTime(courseRequest.getCourseCompTime()); // Set course completion time
+//        course.setCreatedAt(LocalDateTime.now());
+//        course.setUpdatedAt(LocalDateTime.now());
+    }
+
+    @Override
+    public CourseResponse updateCourse(int id, CourseRequest courseRequest) {
+        Course course = courseRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Course not found"));
+        course.setCourseName(courseRequest.getCourseName());
+        course.setCourseCode(courseRequest.getCourseCode());
+        course.setClassSchedule(courseRequest.getClassSchedule());
+        course.setCourseCompTime(courseRequest.getCourseCompTime()); // Update course completion time
+
+        Course updatedCourse = courseRepository.save(course);
+        return convertToCourseResponse(updatedCourse);
+    }
+
+    @Override
+    public void deleteCourse(int id) {
+        Course course = courseRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Course not found"));
+        courseRepository.delete(course);
+    }
+
+    private CourseResponse convertToCourseResponse(Course course) {
+        return new CourseResponse(
+                course.getId(),
+                course.getCourseName(),
+                course.getCourseCode(),
+                course.getClassSchedule(),
+                course.getCourseCompTime()
+        );
     }
 }
