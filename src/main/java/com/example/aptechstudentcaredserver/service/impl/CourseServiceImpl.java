04ae30535f9beb05b19a8aa835cb77acd04d4fbb
Mihpham <minhpham1534@gmail.com>
2024-09-05 11:51:46 +0700

    add message in course

--- src/main/java/com/example/aptechstudentcaredserver/service/impl/CourseServiceImpl.java
+++ src/main/java/com/example/aptechstudentcaredserver/service/impl/CourseServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.aptechstudentcaredserver.entity.Course;
 import com.example.aptechstudentcaredserver.entity.CourseSubject;
 import com.example.aptechstudentcaredserver.entity.Semester;
 import com.example.aptechstudentcaredserver.entity.Subject;
+import com.example.aptechstudentcaredserver.exception.EmptyListException;
 import com.example.aptechstudentcaredserver.exception.NotFoundException;
 import com.example.aptechstudentcaredserver.repository.CourseRepository;
 import com.example.aptechstudentcaredserver.repository.CourseSubjectRepository;
@@ -13,6 +14,7 @@ import com.example.aptechstudentcaredserver.repository.SemesterRepository;
 import com.example.aptechstudentcaredserver.repository.SubjectRepository;
 import com.example.aptechstudentcaredserver.service.CourseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -30,30 +32,51 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public List<CourseResponse> getAllCourses() {
-        return courseRepository.findAll().stream()
-                .map(this::convertToCourseResponse)
-                .collect(Collectors.toList());
+        try {
+            List<Course> courses = courseRepository.findAll();
+            if (courses.isEmpty()) {
+                throw new EmptyListException("No course found.");
+            }
+            return courseRepository.findAll().stream()
+                    .map(this::convertToCourseResponse)
+                    .collect(Collectors.toList());
+        } catch (EmptyListException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to retrieve course.", e);
+        }
     }
 
     @Override
-    public CourseResponse getCourseById(int id) {
-        Course course = courseRepository.findById(id)
+    public CourseResponse getCourseById(int courseId) {
+        Course course = courseRepository.findById(courseId)
                 .orElseThrow(() -> new NotFoundException("Course not found"));
         return convertToCourseResponse(course);
     }
 
     @Override
     public void createCourse(CourseRequest courseRequest) {
-        Course course = new Course();
-        course.setCourseName(courseRequest.getCourseName());
-        course.setCourseCode(courseRequest.getCourseCode());
-        course.setCourseCompTime(courseRequest.getCourseCompTime());
-        course.setCreatedAt(LocalDateTime.now());
-        course.setUpdatedAt(LocalDateTime.now());
+        try {
+            if (courseRequest.getCourseName() == null || courseRequest.getCourseName().isEmpty()) {
+                throw new IllegalArgumentException("Subject name cannot be null or empty.");
+            }
 
-        addSubjectsToCourse(course, courseRequest.getSubjectsPerSemester());
+            Course course = new Course();
+            course.setCourseName(courseRequest.getCourseName());
+            course.setCourseCode(courseRequest.getCourseCode());
+            course.setCourseCompTime(courseRequest.getCourseCompTime());
+            course.setCreatedAt(LocalDateTime.now());
+            course.setUpdatedAt(LocalDateTime.now());
 
-        courseRepository.save(course);
+            addSubjectsToCourse(course, courseRequest.getSubjectsPerSemester());
+
+            courseRepository.save(course);
+        } catch (
+                DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("Course with the same name already exists.", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create subject.", e);
+        }
     }
 
     private void addSubjectsToCourse(Course course, Map<Integer, List<Integer>> subjectsPerSemester) {
@@ -82,29 +105,36 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public CourseResponse updateCourse(int courseId, CourseRequest courseRequest) {
-        Course course = courseRepository.findById(courseId)
-                .orElseThrow(() -> new NotFoundException("Course not found"));
+        try {
+            Course course = courseRepository.findById(courseId)
+                    .orElseThrow(() -> new NotFoundException("Course with " + courseId + " not found"));
 
-        course.setCourseName(courseRequest.getCourseName());
-        course.setCourseCode(courseRequest.getCourseCode());
-        course.setCourseCompTime(courseRequest.getCourseCompTime());
-        course.setUpdatedAt(LocalDateTime.now());
+            course.setCourseName(courseRequest.getCourseName());
+            course.setCourseCode(courseRequest.getCourseCode());
+            course.setCourseCompTime(courseRequest.getCourseCompTime());
+            course.setUpdatedAt(LocalDateTime.now());
 
-        // Clear old course-subject relations
-        course.getCourseSubjects().clear();
-        courseSubjectRepository.deleteById(courseId);
+            // Clear old course-subject relations
+            course.getCourseSubjects().clear();
+            courseSubjectRepository.deleteById(courseId);
 
-        // Add new subjects per semester
-        addSubjectsToCourse(course, courseRequest.getSubjectsPerSemester());
+            // Add new subjects per semester
+            addSubjectsToCourse(course, courseRequest.getSubjectsPerSemester());
 
-        Course updatedCourse = courseRepository.save(course);
-        return convertToCourseResponse(updatedCourse);
+            Course updatedCourse = courseRepository.save(course);
+            return convertToCourseResponse(updatedCourse);
+        } catch (NotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to update course.", e);
+        }
+
     }
 
     @Override
     public void deleteCourse(int courseId) {
         Course course = courseRepository.findById(courseId)
-                .orElseThrow(() -> new NotFoundException("Course not found"));
+                .orElseThrow(() -> new NotFoundException("Course with " + courseId + " not found"));
         courseRepository.delete(course);
     }
 
