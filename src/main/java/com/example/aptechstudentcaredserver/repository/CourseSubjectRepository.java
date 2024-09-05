3e4c2769d9cc1edd5d0977c2ede65b8ab9ae8932
Quang Doan <126041833+DoanVietQuang@users.noreply.github.com>
2024-09-05 16:00:52 +0700

    fix add course api

--- src/main/java/com/example/aptechstudentcaredserver/repository/CourseSubjectRepository.java
+++ src/main/java/com/example/aptechstudentcaredserver/repository/CourseSubjectRepository.java
@@ -3,7 +3,10 @@ package com.example.aptechstudentcaredserver.repository;
 import com.example.aptechstudentcaredserver.entity.CourseSubject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CourseSubjectRepository extends JpaRepository<CourseSubject, Integer> {
+    List<CourseSubject> findByCourseId(int courseId);
+
 }
