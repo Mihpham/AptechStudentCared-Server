0ebe1a096e6e6113b712781b6123fe3fc4505002
Mihpham <minhpham1534@gmail.com>
2024-09-09 15:35:02 +0700

    update controller message

--- src/main/java/com/example/aptechstudentcaredserver/controller/CourseController.java
+++ src/main/java/com/example/aptechstudentcaredserver/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.example.aptechstudentcaredserver.controller;
 import com.example.aptechstudentcaredserver.bean.request.CourseRequest;
 import com.example.aptechstudentcaredserver.bean.response.CourseResponse;
 import com.example.aptechstudentcaredserver.bean.response.ResponseMessage;
+import com.example.aptechstudentcaredserver.exception.DuplicateException;
 import com.example.aptechstudentcaredserver.exception.NotFoundException;
 import com.example.aptechstudentcaredserver.service.CourseService;
 import lombok.RequiredArgsConstructor;
@@ -42,13 +43,27 @@ public class CourseController {
 
     @PutMapping("/{courseId}")
     public ResponseEntity<ResponseMessage> updateCourse(@RequestBody CourseRequest courseRequest, @PathVariable int courseId) {
+        try {
             courseService.updateCourse(courseId, courseRequest);
             return new ResponseEntity<>(new ResponseMessage("Course updated successfully"), HttpStatus.ACCEPTED);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage(e.getMessage()));
+        } catch (DuplicateException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(e.getMessage()));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(e.getMessage()));
+        }
     }
 
     @DeleteMapping("/{courseId}")
     public ResponseEntity<ResponseMessage> deleteCourse(@PathVariable int courseId) {
-        courseService.deleteCourse(courseId);
-        return new ResponseEntity<>(new ResponseMessage("Course deleted successfully"), HttpStatus.ACCEPTED);
+        try {
+            courseService.deleteCourse(courseId);
+            return new ResponseEntity<>(new ResponseMessage("Course deleted successfully"), HttpStatus.ACCEPTED);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage(e.getMessage()));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(e.getMessage()));
+        }
     }
 }
