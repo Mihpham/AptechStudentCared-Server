f142917f3edfa71c77ab2762c5ac9b58b271f1b1
bui thi hoi <hoi.bt.2156@aptechlearning.edu.vn>
2024-09-25 10:31:08 +0700

    Auto gennerate student Rollnumber

--- src/main/java/com/example/aptechstudentcaredserver/bean/request/StudentRequest.java
+++ src/main/java/com/example/aptechstudentcaredserver/bean/request/StudentRequest.java
@@ -16,8 +16,8 @@ public class StudentRequest {
 
     private String image;
 
-    @NotBlank(message = "Roll number is required")
-    private String rollNumber;
+//    @NotBlank(message = "Roll number is required")
+//    private String rollNumber;
 
     @NotBlank(message = "Full name is required")
     @Size(min = 2, max = 100, message = "Full name must be between 2 and 100 characters")
