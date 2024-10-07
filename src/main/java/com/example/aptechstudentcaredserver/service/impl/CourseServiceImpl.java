5e0c0209a5c3f067a01c85d46f9d568d3264785a
DoanQuang <doanquang3645@gmail.com>
2024-10-07 10:29:31 +0700

    nothing

--- src/main/java/com/example/aptechstudentcaredserver/service/impl/CourseServiceImpl.java
+++ src/main/java/com/example/aptechstudentcaredserver/service/impl/CourseServiceImpl.java
@@ -1,9 +1,7 @@
 package com.example.aptechstudentcaredserver.service.impl;
 
 import com.example.aptechstudentcaredserver.bean.request.CourseRequest;
-import com.example.aptechstudentcaredserver.bean.request.SubjectRequest;
 import com.example.aptechstudentcaredserver.bean.response.CourseResponse;
-import com.example.aptechstudentcaredserver.bean.response.SubjectResponse;
 import com.example.aptechstudentcaredserver.entity.Course;
 import com.example.aptechstudentcaredserver.entity.CourseSubject;
 import com.example.aptechstudentcaredserver.entity.Semester;
@@ -16,12 +14,14 @@ import com.example.aptechstudentcaredserver.repository.SemesterRepository;
 import com.example.aptechstudentcaredserver.repository.SubjectRepository;
 import com.example.aptechstudentcaredserver.service.CourseService;
 import com.example.aptechstudentcaredserver.service.SemesterService;
-import com.example.aptechstudentcaredserver.service.SubjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,7 +32,6 @@ public class CourseServiceImpl implements CourseService {
     private final SubjectRepository subjectRepository;
     private final CourseSubjectRepository courseSubjectRepository;
     private final SemesterService semesterService;
-    private final SubjectService subjectService;
 
     @Override
     public List<CourseResponse> getAllCourses() {
@@ -51,7 +50,6 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public void createCourse(CourseRequest request) {
-        // Kiểm tra xem khóa học đã tồn tại hay chưa
         Course existCourseByName = courseRepository.findByCourseName(request.getCourseName());
         if (existCourseByName != null) {
             throw new DuplicateException("Course with name '" + request.getCourseName() + "' already exists");
@@ -66,7 +64,6 @@ public class CourseServiceImpl implements CourseService {
             throw new NotFoundException("At least one semester and subject must be provided");
         }
 
-        // Tạo mới đối tượng Course
         Course course = new Course();
         course.setCourseName(request.getCourseName());
         course.setCourseCode(request.getCourseCode());
@@ -74,14 +71,9 @@ public class CourseServiceImpl implements CourseService {
         course.setCreatedAt(LocalDateTime.now());
         course.setUpdatedAt(LocalDateTime.now());
 
-        // Lưu khóa học vào cơ sở dữ liệu
         course = courseRepository.save(course);
-
-
-        // Lưu thông tin các môn học cho khóa học
         saveCourseSubjects(request, course);
     }
-
 
     @Override
     public CourseResponse updateCourse(int courseId, CourseRequest request) {
@@ -201,52 +193,15 @@ public class CourseServiceImpl implements CourseService {
 
 
     private void saveCourseSubjects(CourseRequest request, Course course) {
-        String[] projectNames = {
-                "Project Semester 1",
-                "Project Semester 2",
-                "Project Semester 3",
-                "Project Semester 4"
-        };
-
-        String[] projectCodes = {
-                "PROJECT_SEM1",
-                "PROJECT_SEM2",
-                "PROJECT_SEM3",
-                "PROJECT_SEM4"
-        };
-
-        for (int i = 0; i < projectNames.length; i++) {
-            Optional<Subject> projectSubjectOpt = subjectRepository.findBySubjectName(projectCodes[i]);
-
-            if (projectSubjectOpt.isEmpty()) {
-                SubjectRequest projectRequest = new SubjectRequest();
-                projectRequest.setSubjectName(projectNames[i]); // Tên môn học theo kỳ
-                projectRequest.setSubjectCode(projectCodes[i]); // Mã môn học theo kỳ
-                projectRequest.setTotalHours(60); // Tổng số giờ học cho môn này
-                subjectService.createSubject(projectRequest);
-                System.out.println("Created new subject: " + projectRequest.getSubjectName());
-            } else {
-                System.out.println("Subject already exists: " + projectSubjectOpt.get().getSubjectName());
-            }
-        }
-
-        request.getSemesters().forEach((semesterName, subjects) -> {
-            boolean hasProjectSubject = subjects.stream()
-                    .anyMatch(subject -> subject.equalsIgnoreCase("PROJECT_" + semesterName));
-            if (!hasProjectSubject) {
-                subjects.add("PROJECT_" + semesterName);
-            }
-        });
         List<CourseSubject> courseSubjectsToSave = new ArrayList<>();
         try {
             processSemestersAndSubjects(request, course, courseSubjectsToSave, false);
             courseSubjectRepository.saveAll(courseSubjectsToSave);
         } catch (NotFoundException e) {
             courseRepository.delete(course);
+            throw e;
         }
     }
-
-
 
     private CourseResponse convertToCourseResponse(Course course) {
         if (course == null) {
